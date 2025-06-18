ba80204877b8646a946f064ce9bec00843d25bfc
Arif3636 <75058626+arifrahman1989@users.noreply.github.com>
2025-06-18 11:38:34 +0600

    Add files via upload

---
+++ Change Color/scripts.js
@@ -0,0 +1,11 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+// const tests = document.querySelector('.test');
+
+buttons.forEach(function(button){
+    button.addEventListener('click', function(e){
+        if(e.target.id){
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+})
